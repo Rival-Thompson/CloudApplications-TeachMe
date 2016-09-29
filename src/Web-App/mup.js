b621cf890303fd01e34fb6df12c8089b159b0976
Rival Thompson <rival.thompson@student.ap.be>
2016-09-29 21:49:13 +0200

    Mockups + verslag

---
+++ src/Web-App/mup.js
@@ -0,0 +1,36 @@
+module.exports = {
+  servers: {
+    one: {
+      host: '54.71.205.83',
+      username: 'ubuntu',
+      pem: './awsserver.pem'
+      // password:
+      // or leave blank for mup authenticate from ssh-agent
+    }
+  },
+
+  meteor: {
+    name: 'Web-App',
+    path: '.',
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+    env: {
+      ROOT_URL: 'http://teachmeapp.ga/',
+      MONGO_URL: 'mongodb://server:aws@ds041546.mlab.com:41546/tachme'
+    },
+
+    deployCheckWaitTime: 60
+  },
+
+  mongo: {
+    oplog: false,
+    port: 27017,
+    servers: {
+      one: {},
+    },
+  },
+};
