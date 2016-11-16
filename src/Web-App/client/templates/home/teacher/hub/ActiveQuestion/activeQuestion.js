fb911eb7d73de9605a095c8443d9bb2187b0e176
Hans Van Staey <s086380@ap.be>
2016-11-16 20:30:28 +0100

    Working basic visualisation open question answers #52 #53 + meteor update - Frontend: activeQuestion.html - Backend: activeQuestion.js - .meteor/..

--- src/Web-App/client/templates/home/teacher/hub/ActiveQuestion/activeQuestion.js
+++ src/Web-App/client/templates/home/teacher/hub/ActiveQuestion/activeQuestion.js
@@ -11,7 +11,7 @@ Template.homeTeacherHubActiveQuestion.helpers({
 
 Template.homeTeacherHubActiveQuestion.rendered = function () {
     var token = Router.current().params.token;
-    Meteor.call("sendActiveQuestion", token, (error, response)=> {
+    Meteor.call("sendActiveQuestion", token, (error, response) => {
         if (error) {
             console.log(error);
         } else {
@@ -19,21 +19,28 @@ Template.homeTeacherHubActiveQuestion.rendered = function () {
             activeQuestion = response.questions[0];
             activeQuestionDep.changed();
 
-            console.log(activeQuestion.answers);
-            for(i = 0; i < activeQuestion.answers.length; i++){
-                answers[i] = activeQuestion.answers[i].antw;
-                console.log(answers[i]);
+            //console.log(activeQuestion.answers);
+            for (i = 0; i < activeQuestion.answers.length; i++) {
+                answers[i] = [activeQuestion.answers[i].antw, Math.floor(Math.random() * 20) + 8];
+                //console.log(answers[i]);
             }
+            console.log(answers);
+
+            WordCloud(document.getElementById('canvas'),
+                {list: answers, backgroundColor: "transparent", wait: 250,
+                    gridSize: Math.round(16 * $('#canvas').height() / 1024)
+                });
         }
     });
 
-    var chart = c3.generate({
-        bindto: '#chart',
-        data: {
-            columns: [
-                ['data1', 30, 200, 100, 400, 150, 250],
-                ['data2', 50, 20, 10, 40, 15, 25]
-            ]
-        }
-    });
+    /*var chart = c3.generate({
+     bindto: '#chart',
+     data: {
+     columns: [
+     ['data1', 30, 200, 100, 400, 150, 250],
+     ['data2', 50, 20, 10, 40, 15, 25]
+     ]
+     }
+     });
+     */
 };
