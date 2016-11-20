c1e22dbb9c237bdd0e7822ab8feac4b5c9a5cca1
Hans Van Staey <s086380@ap.be>
2016-11-20 19:49:24 +0100

    Removing answers of a selected question and ask question makes the selected question active Frontend: - createOpenQuestion.html - createMPQuestion.html - createCodeQuestion.html Backend: - show.js: ask question and remove answers -methods.js: remove answers meteor method and some clean up

--- src/Web-App/server/methods.js
+++ src/Web-App/server/methods.js
@@ -3,8 +3,8 @@
  */
 Meteor.methods({
     sendLesson(token){
-        console.log(Lessons.findOne({"token":token}));
-        return Lessons.findOne({"token":token});
+        console.log(Lessons.findOne({"token": token}));
+        return Lessons.findOne({"token": token});
     },
     sendActiveQuestion(token){
         console.log(Lessons.findOne({"token": token}).activequestion);
@@ -13,14 +13,17 @@ Meteor.methods({
         console.log(Lessons.findOne({"token": token, "questions.num": tempNum}, {fields: {"questions.$": 1}}));
         return Lessons.findOne({"token": token, "questions.num": tempNum}, {fields: {"questions.$": 1}});
     },
-   sendLesson(token){
-       //console.log(Lessons.findOne({"token":token}));
-       return Lessons.findOne({"token":token});
-   },
+    sendLesson(token){
+        //console.log(Lessons.findOne({"token":token}));
+        return Lessons.findOne({"token": token});
+    },
     pushAnswer(args){
-
-       return  Lessons.update({_id:args.id,'questions.num':args.num},{"$push":{'questions.$.answers':{antw:args.answer}}});
-    },getUser(){
+        return Lessons.update({_id: args.id, 'questions.num': args.num},
+            {"$push": {'questions.$.answers': {antw: args.answer}}});
+    }, getUser(){
         return Meteor.user();
+    },
+    removeAnswers(id, num){
+        return Lessons.update({_id: id, "questions.num": num}, {$pull: {"questions.$.answers": []}});
     }
 });
