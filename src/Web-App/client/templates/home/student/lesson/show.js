022aec9334aa35f69760d70e27126091ca5cd75b
Rival-Thompson <s083731@ap.be>
2016-12-09 17:16:50 +0100

    All Student Pages are restyled + BugFix Question Answered #70 #64 - Buttons in navbare are now havving the right style again - When student switches lesson it doesn't take the info from the previous - Restyle of already answered and code page
    - BUGFIX
    -- unsub after redirect or roiute change

--- src/Web-App/client/templates/home/student/lesson/show.js
+++ src/Web-App/client/templates/home/student/lesson/show.js
@@ -1,168 +1,182 @@
 /**
  * Created by Rival on 25/10/2016.
  */
-Meteor.subscribe('user');
-var editor;
-var editor2;
 
-var isAnswered = null;
-var isAnsweredDep = new Tracker.Dependency;
+Deps.autorun(function () {
+    Meteor.subscribe('user');
+});
 
-Template.registerHelper('zelfde', (a, b)=> {
+let editor;
+let editor2;
+
+let isAnswered = null;
+const isAnsweredDep = new Tracker.Dependency;
+
+Template.registerHelper('zelfde', (a, b) => {
     return a == b;
 });
 
-Template.registerHelper('notNull', (a)=> {
+Template.registerHelper('notNull', (a) => {
     return !!(a != null && a != undefined);
 
 
 });
 Template.homeStudentLesson.helpers({
     thisLesson: function () {
-        console.log("selected lesson update");
-        let thislesson =Lessons.findOne();
-        if( parseInt($.cookie('questionnum')) == parseInt(thislesson.activequestion) && $.cookie('lessonId') == thislesson._id ){
-            isAnswered = true;
-            isAnsweredDep.changed();
-        }else {
-            console.log("Got new question");
-            isAnswered = false;
-            isAnsweredDep.changed();
+        //console.log("selected lesson update");
+        const thislesson = Lessons.findOne();
+        if (thislesson != null && thislesson != undefined) {
+            //console.log('ThisLesson ' + $.cookie('lessonId') + ' ==' + thislesson._id);
+            checkQuestionAnswered(thislesson);
         }
         return thislesson;
     },
     selectedquestion: function () {
         //console.log("selected quest update");
-        let thislesson =Lessons.findOne();
-        if( parseInt($.cookie('questionnum')) == parseInt(thislesson.activequestion) ){
-            isAnswered = true;
-            isAnsweredDep.changed();
-        }else {
-            console.log("Got new question");
-            isAnswered = false;
-            isAnsweredDep.changed();
+        const thislesson = Lessons.findOne();
+        if (thislesson != null && thislesson != undefined) {
+            //console.log('selectedQuestion '+$.cookie('lessonId')+' =='+ thislesson._id);
+            checkQuestionAnswered(thislesson);
+
+            const qst = ReactiveMethod.call('getQuestion', {token: thislesson.token, num: thislesson.activequestion});
+            if (editor2 && qst.type == "Code") editor2.setValue(qst.QuestExample);
+            return qst
         }
 
-        let qst =ReactiveMethod.call('getQuestion',{token:thislesson.token,num:thislesson.activequestion});
-        if(editor2 && qst.type == "Code")editor2.setValue(qst.QuestExample);
-        return qst
     },
     qstAnswered: function () {
         isAnsweredDep.depend();
+        //console.log("is this question answered? " + isAnswered);
         return isAnswered;
     }
 
 });
 
 Template.homeStudentLesson.events({
-    "click #HSL_SaveMPAnswer"(event,template){
-        console.log("Saving answers");
+    "click #HSL_SaveMPAnswer"(event, template){
+        //console.log("Saving answers");
 
         let antwoord, beoordeling;
-        let rating = template.find("input[name$='rating']:checked");
-        if(!!rating){
-           //console.log(rating.value);
+        const rating = template.find("input[name$='rating']:checked");
+        if (!!rating) {
+            //console.log(rating.value);
             beoordeling = rating.value;
-        }else{
+        } else {
             //console.log(0);
             beoordeling = 0
         }
 
-        let question = template.find("input[name$='mp']:checked");
-        if(!!question){
+        const question = template.find("input[name$='mp']:checked");
+        if (!!question) {
             //console.log(question.value);
             antwoord = question.value;
-        }else{
+        } else {
             //console.log("NONE");
             antwoord = null;
         }
 
-        if(!!antwoord){
-            let thislesson =Lessons.findOne();
-            let answ = {antw: antwoord,rating: beoordeling};
-            saveAnswer(answ,thislesson._id,thislesson.activequestion);
-        }else {
+        if (!!antwoord) {
+            const thislesson = Lessons.findOne();
+            const answ = {antw: antwoord, rating: beoordeling};
+            saveAnswer(answ, thislesson._id, thislesson.activequestion);
+        } else {
             sAlert.warning("Please select an answer")
         }
 
 
     }
-    ,"click #HSL_SaveOpenAnswer"(event,template){
-        console.log("Saving answers");
+    , "click #HSL_SaveOpenAnswer"(event, template){
+        //console.log("Saving answers");
 
         let antwoord, beoordeling;
-        let rating = template.find("input[name$='rating']:checked");
-        if(!!rating){
-            console.log(rating.value);
+        const rating = template.find("input[name$='rating']:checked");
+        if (!!rating) {
+            //console.log(rating.value);
             beoordeling = rating.value;
-        }else{
-            console.log(0);
+        } else {
+            //console.log(0);
             beoordeling = 0
         }
 
         antwoord = template.find("#HSL-Open-Input").value;
 
-        if (!!antwoord || antwoord!=""){
-            let thislesson =Lessons.findOne();
-            let answ = {antw: antwoord,rating: beoordeling};
-            saveAnswer(answ,thislesson._id,thislesson.activequestion);
-        }else{
+        if (!!antwoord || antwoord != "") {
+            const thislesson = Lessons.findOne();
+            const answ = {antw: antwoord, rating: beoordeling};
+            saveAnswer(answ, thislesson._id, thislesson.activequestion);
+        } else {
             sAlert.warning("Please fill in an answer before submitting");
         }
 
 
     }
-    ,"click #HSL_SaveCodeAnswer"(event,template){
+    , "click #HSL_SaveCodeAnswer"(event, template){
         let antwoord, beoordeling;
-        let rating = template.find("input[name$='rating']:checked");
-        if(!!rating){
-            console.log(rating.value);
+        const rating = template.find("input[name$='rating']:checked");
+        if (!!rating) {
+            //console.log(rating.value);
             beoordeling = rating.value;
-        }else{
-            console.log(0);
+        } else {
+            //console.log(0);
             beoordeling = 0
         }
 
-        if(!!editor){
+        if (!!editor) {
             antwoord = editor.getValue();
-        }else{
+        } else {
             antwoord = null;
         }
 
 
-        if (!!antwoord || antwoord!=""){
-            let thislesson =Lessons.findOne();
-            let answ = {antw: antwoord,rating: beoordeling};
-            saveAnswer(answ,thislesson._id,thislesson.activequestion);
-        }else{
+        if (!!antwoord || antwoord != "") {
+            const thislesson = Lessons.findOne();
+            const answ = {antw: antwoord, rating: beoordeling};
+            saveAnswer(answ, thislesson._id, thislesson.activequestion);
+        } else {
             sAlert.warning("Please fill in an answer before submitting");
         }
     }
-    ,"click #ENDLESSON"(event,tempate){
+    , "click #ENDLESSON"(event, tempate){
         Router.go("homeStudentToken");
     }
 
-    });
+});
 
- saveAnswer = function (answer, lessonid, num) {
-    Meteor.call('pushAnswer',{answer:answer,id:lessonid,num:num},(err,res)=> {
-        if (err){
-            sAlert.error("an error occured while saveing your answer: " +err.message);
-        }else {
+saveAnswer = function (answer, lessonid, num) {
+    Meteor.call('pushAnswer', {answer: answer, id: lessonid, num: num}, (err, res) => {
+        if (err) {
+            sAlert.error("an error occured while saveing your answer: " + err.message);
+        } else {
             sAlert.success("Answer Saved");
-            $.cookie('questionnum', num, { expires: 1, path: '/student/lesson' });
-            $.cookie('lessonId', lessonid, { expires: 1, path: '/student/lesson' });
+            $.cookie('questionnum', num, {expires: 1});
+            $.cookie('lessonId', lessonid, {expires: 1});
             isAnswered = true;
             isAnsweredDep.changed();
         }
     })
 };
 
+checkQuestionAnswered = function (thislesson) {
+    if (!!$.cookie('questionnum') && $.cookie('questionnum') != null && $.cookie('questionnum') != undefined
+        && !!$.cookie('lessonId') && $.cookie('lessonId') != null && $.cookie('lessonId') != undefined) {
+        if (parseInt($.cookie('questionnum')) == parseInt(thislesson.activequestion) && $.cookie('lessonId') == thislesson._id) {
+            isAnswered = true;
+            isAnsweredDep.changed();
+        } else {
+            //console.log("Got new question");
+            isAnswered = false;
+            isAnsweredDep.changed();
+        }
+    }
+};
+
 Template.homeStudentLesson.rendered = function () {
-    var self = this;
-    var token = Router.current().params.token;
-    console.log("the awesome token is: " + token);
-        Meteor.subscribe('studentLesson',token);
+
+    Deps.autorun(function () {
+        const token = Router.current().params.token;
+        //console.log("the awesome token is: " + token);
+        Meteor.subscribe('studentLesson', token);
+    });
 
 };
 
@@ -171,7 +185,7 @@ Template.HSL_CodeQuestion.rendered = function () {
     editor.setTheme("ace/theme/cobalt");
     editor.getSession().setMode("ace/mode/javascript");
     editor.setValue("");
-    console.log("editting: " + editor);
+    //console.log("editting: " + editor);
 
     editor2 = ace.edit("editor2");
     editor2.setTheme("ace/theme/monokai");
