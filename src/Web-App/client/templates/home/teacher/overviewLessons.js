1ba7e91bb8c26d678dcc0061b77fc9d863a88984
Rival-Thompson <s083731@ap.be>
2016-12-10 19:16:34 +0100

    prepping overview lessonspage

---
+++ src/Web-App/client/templates/home/teacher/overviewLessons.js
@@ -0,0 +1,14 @@
+/**
+ * Created by rival on 10/12/2016.
+ */
+Template.homeTeacherOverviewLesson.helpers({
+
+});
+
+Template.homeTeacherOverviewLesson.events({
+
+});
+
+Template.homeTeacherOverviewLesson.rendered = function () {
+
+};
